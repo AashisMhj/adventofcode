fd121fa934462d48665fada188cb86f53d51c25f
Aashis Maharjan <aashismhj5511@gmail.com>
2023-05-18 19:34:49 +0545

    day 8 part 1 completed

---
+++ day_8/8.go
@@ -0,0 +1,168 @@
+package main
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"strconv"
+)
+const FILE_PATH string = "./input5.txt"
+
+func main(){
+	count := 0
+	content, err := ioutil.ReadFile(FILE_PATH)
+	if err != nil{
+		fmt.Printf("Error reading file: %v", err)
+	}
+	lines :=  strings.Split(string(content), "\n")
+
+	var multiArray [][]int
+
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if line != ""{
+			chars := strings.Split(line, "")
+			intArray := make([]int, len(chars))
+			for index, value := range chars{
+				num, err := strconv.Atoi(value)
+				if err != nil{
+					fmt.Printf("Error converting element at index %d: %s \n", index, err.Error())
+					continue
+				}
+				intArray[index] =num
+			}
+			multiArray = append(multiArray, intArray)
+		}
+	}
+
+	highestCountTop := multiArray[0]
+	highestCountBottom := multiArray[len(multiArray)-1]
+	highestCountLeft := make([]int, len(multiArray))
+	highestCountRight := make([]int, len(multiArray))
+	for i:= 0; i < len(multiArray); i++{
+		highestCountLeft[i] = multiArray[i][0]
+		highestCountRight[i] = multiArray[i][len(multiArray)-1]
+	}
+	
+	treesMap := make(map[string]int)
+
+	
+
+	for i := len(multiArray) -2; i >= 1; i--{
+		for j := len(multiArray[i]) -2; j >= 1; j--{
+			current_tree := multiArray[i][j]
+			if(current_tree > highestCountRight[i]){
+				map_key := string(fmt.Sprint(strconv.Itoa(i), "-", strconv.Itoa(j)))
+				highestCountRight[i] = current_tree
+				_, exists := treesMap[map_key]
+				if !exists{
+					treesMap[map_key] = current_tree
+					count += 1
+				} 
+			}
+			if(current_tree > highestCountBottom[j]){
+				map_key := string(fmt.Sprint(strconv.Itoa(i), "-", strconv.Itoa(j)))
+				highestCountBottom[j] = current_tree
+				_, exists := treesMap[map_key]
+				if !exists{
+					treesMap[map_key] = current_tree
+					count += 1
+				}
+			}
+		}
+	}
+	
+
+	// checking top
+	for i:=1; i <= len(multiArray)-2; i++{
+		for j:=1; j <= len(multiArray[i])-2; j++{
+			current_tree := multiArray[i][j]
+			if(current_tree > highestCountTop[j]){
+				map_key := string(fmt.Sprint(strconv.Itoa(i), "-", strconv.Itoa(j)))
+				highestCountTop[j] = current_tree
+				_, exists := treesMap[map_key]
+				if !exists{
+					treesMap[map_key] = current_tree
+					count += 1
+				}
+			}
+			if(multiArray[i][j] > highestCountLeft[i]){
+				map_key := string(fmt.Sprint(strconv.Itoa(i), "-", strconv.Itoa(j)))
+				highestCountLeft[i] = current_tree
+				_, exists := treesMap[map_key]
+				if !exists{
+					treesMap[map_key] = current_tree
+					count += 1
+				}
+			}
+		}
+	}
+
+	lengthX := (len(multiArray[0])) * 2
+	lengthY := (len(multiArray) -2) * 2
+	
+
+	fmt.Printf("Total no of visible trees %d \n", count + lengthX + lengthY)
+
+	highest_point := 0;
+	location_i := 0
+	location_j := 0
+	
+
+	fmt.Println(multiArray)
+	// part two calcualte the senic point
+	for i := 1; i <= len(multiArray)-2; i++{
+		for j := 1; j <= len(multiArray[i])-2; j++{
+			
+			current_tree := multiArray[i][j]
+			view_count := 0
+			fmt.Println(current_tree)
+			for a:= i-1; a >=0; a--{
+				fmt.Println("view count",view_count)
+				fmt.Println("a",a)
+				fmt.Println("j value",j)
+				fmt.Println("array vlaue",multiArray[a][j])
+				if(current_tree <= multiArray[a][j]){
+					view_count += 1
+					break
+				}else{
+					view_count += 1
+				}
+			}
+			// for b:= i+1; b < len(multiArray); b++{
+			// 	if(current_tree <= multiArray[b][j]){
+			// 		view_count += 1
+			// 		break
+			// 	}else{
+			// 		view_count += 1
+			// 	}
+				
+			// }
+			// for c:= j-1; c >=0; c--{
+			// 	if(current_tree <= multiArray[i][c]){
+			// 		view_count += 1
+			// 		break
+			// 	}else{
+			// 		view_count += 1
+			// 	}
+			// }
+			// for d:= j+1; d < len(multiArray); d++{
+			// 	if(current_tree <= multiArray[i][d]){
+			// 		view_count += 1
+			// 		break
+			// 	}else{
+			// 		view_count += 1
+			// 	}
+				
+			// }
+
+			if(view_count > highest_point){
+				location_i = i
+				location_j = j
+				highest_point = view_count
+			}
+
+		}
+	}	
+
+	fmt.Printf("Highest view point: %d, location [%d][%d] \n", highest_point, location_i, location_j)
+}
